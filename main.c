83b77fcca8cce3bef7ba0743d15cf4e1b1fdad96
thesahiljoseph <thesahiljoseph@gmail.com>
2022-12-23 02:18:06 +0500

    first commit

---
+++ main.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+    // // // // // // // // // //
+    
+    // Data Structures and Algorithms - Introduction
+    // 
+
+    // What is Data Structures?
+    // 
+
+    // What is Algorithms?
+    // 
+
+    // Why We use Data Structures and Algorithms?
+    // 
+
+    // // // // // // // // // //
+
+    return 0;
+}
